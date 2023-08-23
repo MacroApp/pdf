dbcec116d25096b5d77043ba4b1fe057e63cacda
AIWASS23 <marcelodearaujo677@gmail.com>
2023-08-23 11:41:24 -0300

    POC's sobre gerar e exporta PDF

---
+++ PDF/SharedData.swift
@@ -0,0 +1,18 @@
+//
+//  SharedData.swift
+//  PDF
+//
+//  Created by Marcelo De Araújo on 23/08/23.
+//
+
+import Foundation
+
+class SharedData: ObservableObject {
+
+    // MARK: PDF Properties
+    // ShareSheet producing error when using @State
+    // Workaround: Use @StateObject instead
+    
+    @Published var PDFUrl: URL?
+    @Published var showShareSheet: Bool = false
+}
