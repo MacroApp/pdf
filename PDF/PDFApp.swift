1321dbe252785ecc88da311a44fa2cc7a201693e
AIWASS23 <marcelodearaujo677@gmail.com>
2023-08-23 09:09:45 -0300

    Initial Commit

---
+++ PDF/PDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFApp.swift
+//  PDF
+//
+//  Created by Marcelo De Araújo on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
